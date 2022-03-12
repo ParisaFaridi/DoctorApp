8cd46abed5d2f5b397441b070e7ffc2a539451c4
Pareeesa <parisa.faridi80@gmail.com>
2022-03-12 15:09:56 +0330

    Display info of selected doctor

--- app/src/main/java/com/example/doctorapp2/DoctorFragment.kt
+++ app/src/main/java/com/example/doctorapp2/DoctorFragment.kt
@@ -11,24 +11,34 @@ import com.example.doctorapp2.databinding.FragmentDoctorBinding
 
 class DoctorFragment : Fragment() {
 
-    val doctorViewModel : DoctorViewModel by viewModels()
-    private val args :DoctorFragmentArgs by navArgs()
+    private val doctorViewModel: DoctorViewModel by viewModels()
+    private val args: DoctorFragmentArgs by navArgs()
 
     private var _binding: FragmentDoctorBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
+        savedInstanceState: Bundle?
+    ): View {
 
         _binding = FragmentDoctorBinding.inflate(inflater, container, false)
-        return  binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        doctorViewModel.testData()
         val id = args.docId
+        binding.apply {
+            tvName.text = doctorViewModel.doctorList[id].name
+            tvProfession.text = doctorViewModel.doctorList[id].profession
+            tvPhone.text = doctorViewModel.doctorList[id].phone
+            tvCity.text = doctorViewModel.doctorList[id].address
+            tvStatus.text = doctorViewModel.doctorList[id].status.toString()
+        }
+        activity?.title = doctorViewModel.doctorList[id].name
 
 
     }
