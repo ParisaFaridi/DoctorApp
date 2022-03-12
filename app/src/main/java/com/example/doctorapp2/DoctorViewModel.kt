701da1a413d5e494acb2cead60798d7343554899
Pareeesa <parisa.faridi80@gmail.com>
2022-03-12 14:00:42 +0330

    pass data to DoctorFragment via viewModel

---
+++ app/src/main/java/com/example/doctorapp2/DoctorViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.doctorapp2
+
+import androidx.lifecycle.ViewModel
+
+class DoctorViewModel:ViewModel() {
+
+    var doctorList= arrayListOf<Doctor>()
+
+    fun testData(){
+        doctorList.clear()
+        val doc1=Doctor("امیر احسانی",0,"تهران","متخصص پوست",
+            "44852347")
+        val doc2=Doctor("محمد صادقیان",1,"تهران","ارتوپد",
+            "66889925")
+        val doc3=Doctor("زهرا حسینی",2,"تهران","روان پزشک",
+            "22123456")
+
+        doctorList.add(doc1)
+        doctorList.add(doc2)
+        doctorList.add(doc3)
+
+    }
+}
