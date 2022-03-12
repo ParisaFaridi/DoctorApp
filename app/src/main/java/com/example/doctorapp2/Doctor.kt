c9c0465c99eaad454a516d0bafc00d95c7798fcc
Pareeesa <parisa.faridi80@gmail.com>
2022-03-12 15:09:13 +0330

    Add status property to Doctor.kt

--- app/src/main/java/com/example/doctorapp2/Doctor.kt
+++ app/src/main/java/com/example/doctorapp2/Doctor.kt
@@ -5,4 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Doctor(var name:String, var id:Int, var address:String, var profession:String,
-                  var phone: String):Parcelable
+                  var phone: String,var status:Status):Parcelable
+enum class Status{
+    Online,Offline
+}
--- app/src/main/java/com/example/doctorapp2/DoctorNameViewModel.kt
+++ app/src/main/java/com/example/doctorapp2/DoctorNameViewModel.kt
@@ -9,11 +9,11 @@ class DoctorNameViewModel:ViewModel() {
     fun testData(){
         doctorList.clear()
         val doc1=Doctor("امیر احسانی",0,"تهران","متخصص پوست",
-            "44852347")
+            "44852347",Status.Online)
         val doc2=Doctor("محمد صادقیان",1,"تهران","ارتوپد",
-            "66889925")
+            "66889925",Status.Online)
         val doc3=Doctor("زهرا حسینی",2,"تهران","روان پزشک",
-            "22123456")
+            "22123456",Status.Online)
 
         doctorList.add(doc1)
         doctorList.add(doc2)
