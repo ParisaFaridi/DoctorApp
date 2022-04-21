9cd6e09e5f5029393133f07db14be28064a7b7f6
Pareeesa <parisa.faridi80@gmail.com>
2022-04-21 11:57:43 +0430

    Give dataTest to adapter from viewModel

--- app/src/main/java/com/example/doctorapp2/DoctorListFragment.kt
+++ app/src/main/java/com/example/doctorapp2/DoctorListFragment.kt
@@ -1,12 +1,11 @@
 package com.example.doctorapp2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.example.doctorapp2.databinding.FragmentDoctorListBinding
 
 class DoctorListFragment : Fragment() {
@@ -31,31 +30,26 @@ class DoctorListFragment : Fragment() {
 
         activity?.title = "Doctor App"
 
-        val testData = listOf(
-            Doctor("امیر احسانی", 0, "تهران", "متخصص پوست", "44852347", Status.Online),
-            Doctor("محمد صادقیان", 1, "تهران", "ارتوپد", "66889925", Status.Online),
-            Doctor("زهرا حسینی", 2, "تهران", "روان پزشک", "22123456", Status.Offline)
-        )
-        val adapter = DoctorAdapter(testData)
+        doctorViewModel.testData()
+        val adapter = DoctorAdapter(doctorViewModel.doctorList)
         binding.doctorRv.adapter = adapter
         adapter.notifyDataSetChanged()
 
-        doctorViewModel.testData()
-        binding.btnDoc1.text =doctorViewModel.doctorList[0].name
-        binding.btnDoc2.text =doctorViewModel.doctorList[1].name
-        binding.btnDoc3.text =doctorViewModel.doctorList[2].name
-
-        binding.apply {
-            btnDoc1.setOnClickListener {navigateToSecondFragment(0)}
-            btnDoc2.setOnClickListener {navigateToSecondFragment(1)}
-            btnDoc3.setOnClickListener {navigateToSecondFragment(2)}
-        }
+//        binding.btnDoc1.text =doctorViewModel.doctorList[0].name
+//        binding.btnDoc2.text =doctorViewModel.doctorList[1].name
+//        binding.btnDoc3.text =doctorViewModel.doctorList[2].name
+//
+//        binding.apply {
+//            btnDoc1.setOnClickListener {navigateToSecondFragment(0)}
+//            btnDoc2.setOnClickListener {navigateToSecondFragment(1)}
+//            btnDoc3.setOnClickListener {navigateToSecondFragment(2)}
+//        }
     }
 
-    private fun navigateToSecondFragment(id: Int) {
-        findNavController().navigate(
-            DoctorListFragmentDirections
-                .actionDoctorListFragmentToDoctorFragment(id)
-        )
-    }
+//    private fun navigateToSecondFragment(id: Int) {
+//        findNavController().navigate(
+//            DoctorListFragmentDirections
+//                .actionDoctorListFragmentToDoctorFragment(id)
+//        )
+//    }
 }
