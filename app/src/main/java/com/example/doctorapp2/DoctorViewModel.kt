e3f0fad730f02d497ab53e18ed7c2c452b28a2c0
Pareeesa <parisa.faridi80@gmail.com>
2022-04-21 12:01:26 +0430

    Add more testData

--- app/src/main/java/com/example/doctorapp2/DoctorViewModel.kt
+++ app/src/main/java/com/example/doctorapp2/DoctorViewModel.kt
@@ -14,10 +14,25 @@ class DoctorViewModel:ViewModel() {
             "66889925",Status.Online)
         val doc3=Doctor("زهرا حسینی",2,"تهران","روان پزشک",
             "22123456",Status.Offline)
+        val doc4=Doctor("محمد محمدی",3,"تهران","روان پزشک",
+            "22123456",Status.Offline)
+        val doc5=Doctor("فاطمه صادقی",4,"تهران","روان پزشک",
+            "22123456",Status.Offline)
+        val doc6=Doctor("g",5,"تهران","روان پزشک",
+            "22123456",Status.Offline)
+        val doc7=Doctor("زهرا",6,"تهران","روان پزشک",
+            "22123456",Status.Offline)
+        val doc8=Doctor("حسینی",7,"تهران","روان پزشک",
+            "22123456",Status.Offline)
 
         doctorList.add(doc1)
         doctorList.add(doc2)
         doctorList.add(doc3)
+        doctorList.add(doc4)
+        doctorList.add(doc5)
+        doctorList.add(doc6)
+        doctorList.add(doc7)
+        doctorList.add(doc8)
 
     }
 }
